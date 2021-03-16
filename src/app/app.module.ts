2a8c4e779d69f6f6eec47fbee6a13c9997fef3c6
Ali Atakay <aliatakay@gmail.com>
2021-03-16 22:07:00 +0300

    Created Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    SidebarComponent,
+    NavigationComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
