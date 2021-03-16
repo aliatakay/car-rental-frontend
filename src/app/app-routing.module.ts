eb230248b77436e71e78c3e0537c5979c7d61d1a
Ali Atakay <aliatakay@gmail.com>
2021-03-17 01:48:24 +0300

    Added Routing for Brand-Color Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'brands', component: BrandComponent },
+  { path: 'colors', component: ColorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
