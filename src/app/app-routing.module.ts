5d9a043a74afb7eff174eb4a13a954aec91e1c58
Ali Atakay <aliatakay@gmail.com>
2021-03-17 16:12:40 +0300

    Added All Files for Customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandComponent } from './components/brand/brand.component';
 import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 const routes: Routes = [
   { path: 'brands', component: BrandComponent },
   { path: 'colors', component: ColorComponent },
+  { path: 'customers', component: CustomerComponent },
 ];
 
 @NgModule({
